e1691d6970074f91938973afe24ae1a60b48b734
KohnoA <san.kohno@yandex.ru>
2022-08-02 03:55:46 +0300

    feat: task conmpleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for(let i = 0; i < str.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      let currentBracker = str[i];
+      let stackTop = stack[stack.length - 1];
+      const bracketsPair = {
+        [bracketsConfig[j][1]]: bracketsConfig[j][0] // ')': '('
+      }
+
+      if(bracketsPair[currentBracker] === stackTop && stack.length > 0) {
+        stack.pop();
+      } else if(bracketsConfig[j].includes(currentBracker)) {
+        stack.push(currentBracker);
+      }      
+    }
+  }
+  
+  return stack.length === 0;
 }
